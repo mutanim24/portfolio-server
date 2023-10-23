0f23c6c998adb591711545038115b3d4b3988ed0
mutanim24 <mutanim24@gmail.com>
2023-10-23 15:39:32 +0600

    setupcomplete the server and post and project uploaded

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = 3000;
+require('dotenv').config()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// 
+// 
+
+// console.log(process.env.DB_USER)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z12trsh.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const blogCollection = client.db("Portfolio").collection("blog");
+        const projectCollection = client.db("Portfolio").collection("projects");
+
+        app.get("/blog", async (req, res) =>{
+            const result = await blogCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get("/projects", async (req, res) =>{
+            const result = await projectCollection.find().toArray();
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
